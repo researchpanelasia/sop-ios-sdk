4c3b963c4b79501bbe2f8303b767425d153cd267
solnime <solnime@gmail.com>
2017-02-28 16:57:25 +0900

    commit base project

---
+++ SurveyonPartners/SurveyonPartners.h
@@ -0,0 +1,19 @@
+//
+//  SurveyonPartners.h
+//  SurveyonPartners
+//
+//  Created by Choi Jiseon on 2017/02/23.
+//  Copyright © 2017年 d8aspring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SurveyonPartners.
+FOUNDATION_EXPORT double SurveyonPartnersVersionNumber;
+
+//! Project version string for SurveyonPartners.
+FOUNDATION_EXPORT const unsigned char SurveyonPartnersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurveyonPartners/PublicHeader.h>
+
+
---
+++ SurveyonPartners/SurveyonPartners.swift
@@ -0,0 +1,21 @@
+//
+//  SurveyonPartners.swift
+//  SurveyonPartners
+//
+//  Created by Choi Jiseon on 2017/02/23.
+//  Copyright © 2017年 d8aspring. All rights reserved.
+//
+
+import Foundation
+
+public class SurveyonPartners {
+    
+    public class func setUp() {
+        print("Start setUp func.")
+    }
+    
+    public class func showSurveyList() {
+        print("Start showSurveyList func.")
+    }
+    
+}
