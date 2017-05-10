18d8c4b893ae3a3f506eeca1cdce7338121d273c
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-05-10 17:21:42 +0900

    add profiling item factory

--- SurveyonPartners/models/SurveyListItemFactory.swift
+++ SurveyonPartners/models/SurveyListItemFactory.swift
@@ -7,54 +7,26 @@
 
 class SurveyListItemFactory {
   
-  static func create(data: Data) -> [SurveyListItem] {
+  static func create(data: Data) throws -> [SurveyListItem] {
     
-    var surveyListArray: [SurveyListItem] = []
-    
-    do {
-      let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
-      if let dataJson = json!["data"] as? [String: Any] {
-        if let profiling = dataJson["profiling"] as? [[String: Any]] {
-          for index in 0..<profiling.count {
-            let profilingData = ImplProfiling(name: profiling[index]["name"] as? String,
-                                                  title: profiling[index]["title"] as? String,
-                                                  url: profiling[index]["url"] as? String)
-            surveyListArray.append(profilingData)
-          }
-        }
+    var surveyListItems: [SurveyListItem] = []
+
+    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:AnyObject]
+    guard let profilings = (json?["data"]?["profiling"] as? [[String:AnyObject]]) else {
+      throw ProfilingInvalidDataError()
+    }
+
+    for profile in profilings {
+      do {
+        try surveyListItems.append(ImplProfiling(json: profile))
+      } catch is ProfilingInvalidDataError {
+        SOPLog.info(message: "Invalid data in profiling")
+      } catch let e {
+        throw e
       }
-    } catch let error as NSError {
-      SOPLog.error(message: error.localizedDescription)
     }
     
-    do {
-      let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
-      if let dataJson = json!["data"] as? [String: Any] {
-        if let research = dataJson["research"] as? [[String: Any]] {
-          for index in 0..<research.count {
-//            let researchData = ImplResearch(surveyId: research[index]["survey_id"] as? String,
-//                                            quotaId: research[index]["quota_id"] as? String,
-//                                            cpi: research[index]["cpi"] as? String,
-//                                            ir: research[index]["ir"] as? String,
-//                                            loi: research[index]["loi"] as? String,
-//                                            isAnswered: research[index]["is_answered"] as? String,
-//                                            isClosed: research[index]["is_closed"] as? String,
-//                                            title: research[index]["title"] as? String,
-//                                            url: research[index]["url"] as? String,
-//                                            isFixedLoi: research[index]["is_fixed_loi"] as? String,
-//                                            isNotifiable: research[index]["is_notifiable"] as? String,
-//                                            date: research[index]["date"] as? String,
-//                                            blockedDevices: research[index]["blocked_devices"] as? String,
-//                                            extraInfo: research[index]["extra_info"] as? String)
-//            surveyListArray.append(researchData)
-          }
-        }
-      }
-    } catch let error as NSError {
-      SOPLog.error(message: error.localizedDescription)
-    }
-    
-    return surveyListArray
+    return surveyListItems
   }
   
 }
---
+++ SurveyonPartnersTests/SurveyListItemFactoryTests.swift
@@ -0,0 +1,72 @@
+//
+//  SurveyListItemFactoryTests.swift
+//  SurveyonPartners
+//
+//  Copyright © 2017年 d8aspring. All rights reserved.
+//
+
+import XCTest
+@testable import SurveyonPartners
+
+class SurveyListItemFactoryTests: XCTestCase {
+
+  func testCreate() throws{
+    let response = "{" +
+      "  \"meta\": {" +
+      "    \"code\": 200," +
+      "    \"message\": \"\"" +
+      "  }," +
+      "  \"data\": {" +
+      "    \"profiling\": [" +
+      "      {" +
+      "        \"name\": \"q001\"," +
+      "        \"title\": \"请问您的生日是多少?\"," +
+      "        \"url\": \"https://partners.surveyon.com/resource/auth/v1_1\"" +
+      "      }" +
+      "    ]," +
+      "    \"research\": [" +
+      "      {" +
+      "        \"survey_id\": \"10000\"," +
+      "        \"quota_id\": \"20000\"," +
+      "        \"cpi\": \"1.23\"," +
+      "        \"ir\": \"80\"," +
+      "        \"loi\": \"10\"," +
+      "        \"is_answered\": \"0\"," +
+      "        \"is_closed\": \"0\"," +
+      "        \"title\": \"Example Research Survey\"," +
+      "        \"url\": \"https://partners.surveyon.com/resource/auth/v1_1\"," +
+      "        \"is_fixed_loi\": \"1\"," +
+      "        \"is_notifiable\": \"1\"," +
+      "        \"date\": \"2015-01-01\"," +
+      "        \"blocked_devices\": {" +
+      "          \"PC\": 1" +
+      "        }," +
+      "        \"extra_info\": { }" +
+      "      }," +
+      "      {" +
+      "        \"survey_id\": \"10002\"," +
+      "        \"quota_id\": \"20002\"," +
+      "        \"cpi\": \"2.34\"," +
+      "        \"ir\": \"90\"," +
+      "        \"loi\": \"10\"," +
+      "        \"is_answered\": \"0\"," +
+      "        \"is_closed\": \"1\"," +
+      "        \"title\": \"Example Research Survey\"," +
+      "        \"url\": \"\"," +
+      "        \"is_fixed_loi\": \"0\"," +
+      "        \"is_notifiable\": \"0\"," +
+      "        \"date\": \"2015-01-03\"," +
+      "        \"blocked_devices\": {" +
+      "          \"MOBILE\": 1," +
+      "          \"TABLET\": 1" +
+      "        }," +
+      "        \"extra_info\": { }" +
+      "      }" +
+      "    ]" +
+      "  }" +
+    "}"
+
+    let items = try SurveyListItemFactory.create(data: response.data(using: .utf8)!)
+    XCTAssertEqual(items[0].title, "请问您的生日是多少?")
+  }
+}
